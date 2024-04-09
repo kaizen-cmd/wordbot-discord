6e11d9f4669a138d6bbb6577b58f731f0c4fb266
Tejas Mandre <tmandre@vmware.com>
2024-04-09 15:04:21 +0530

    db reset, slash commands added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sqlite3
 import discord
 import os
+from discord.ext import commands
+from discord import app_commands
+import requests
 
 CHANNEL_ID = 1225453961751035974
 
@@ -31,7 +34,7 @@ class WordGameDB:
             for word in f.readlines():
                 word = word.strip()
                 if len(word) >= 2:
-                    words.append((word, ))
+                    words.append((word,))
             self.curr.executemany("INSERT INTO words (word) VALUES (?)", words)
             self.conn.commit()
 
@@ -53,6 +56,24 @@ class WordGameDB:
         self.conn.commit()
         return score
 
+    def deduct_score(self, message):
+        user = message.author
+        points = len(message.content)
+        score = self.curr.execute(
+            f"SELECT score FROM users WHERE id=?", (user.id,)
+        ).fetchone()
+        if not score:
+            self.curr.execute(
+                "INSERT INTO users(id, username, servername) VALUES (?, ?, ?)",
+                (user.id, user.name, user.display_name),
+            )
+            self.conn.commit()
+            score = (0,)
+        score = max(0, score[0] - 2)
+        self.curr.execute("UPDATE users SET score=? WHERE id=?", (score, user.id))
+        self.conn.commit()
+        return score
+
     def try_play_word(self, message):
         word = message.content.lower()
         user = message.author
@@ -70,10 +91,10 @@ class WordGameDB:
             return "NYT"  # Not this user's turn
         elif not db_word:
             return "WND"  # Word not in the dictionary
-        elif lastchar and word[0] != lastchar:
-            return "WFCNM"  # Word's first character does not match the last character
         elif db_word[1] == 1:
             return "WAU"  # Word is already used
+        elif lastchar and word[0] != lastchar:
+            return "WFCNM"  # Word's first character does not match the last character
         else:
             self.curr.execute("UPDATE words SET isUsed=1 WHERE word=?", (word,))
             self.curr.execute("UPDATE last_char SET lastchar=? WHERE id=1", (word[-1],))
@@ -84,16 +105,19 @@ class WordGameDB:
             return "WA"  # Accept word
 
 
-class WordGameClient(discord.Client):
+class WordGameClient(commands.Bot):
     def __init__(self, db, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.db: WordGameDB = db
 
     async def on_ready(self):
         print(f"We have logged in as {self.user}")
+        synced = await self.tree.sync()
+        print(synced)
 
-    def validate_message(self, message):
-        if message.author == self.user or message.channel.id != CHANNEL_ID:
+    @staticmethod
+    def validate_message(message, bot_user=None):
+        if bot_user and message.author == bot_user or message.channel.id != CHANNEL_ID:
             return False
         words = message.content.lower().split(" ")
         if len(words) > 1:
@@ -102,13 +126,13 @@ class WordGameClient(discord.Client):
         return word.isalpha()
 
     async def on_message(self, message: discord.message.Message):
-        if self.validate_message(message):
+        if self.validate_message(message, self.user):
             result = self.db.try_play_word(message)
             if result == "WA":
                 score = self.db.update_score(message)
                 await message.add_reaction("✅")
                 await message.channel.send(
-                    content=f"+{len(message.content)} points! {message.author.display_name}'s score {score}"
+                    content=f"🎉 +**{len(message.content)} points!** {message.author.display_name}'s score **{score}** "
                 )
             elif result == "NYT":
                 await message.channel.send("Not your turn")
@@ -120,8 +144,9 @@ class WordGameClient(discord.Client):
                 await message.channel.send("Word not in the dictionary")
             elif result == "WFCNM":
                 await message.add_reaction("❌")
+                score = self.db.deduct_score(message)
                 await message.channel.send(
-                    "Check the last character of the previous word"
+                    f"Check the last character of the previous word, **2 points** deducted 😔. Score: {score}"
                 )
 
 
@@ -137,5 +162,46 @@ if __name__ == "__main__":
     intents = discord.Intents.default()
     intents.messages = True
     intents.message_content = True
-    client = WordGameClient(db, intents=intents)
+    client = WordGameClient(db, intents=intents, command_prefix="!")
+
+    @client.tree.command(name="score")
+    async def score(ctx: discord.Interaction):
+        QUERY = "SELECT servername, score FROM users ORDER BY score DESC LIMIT 5"
+        user_rows = db.curr.execute(QUERY).fetchall()
+        message = "```\n"
+        rank = 1
+        for user_row in user_rows:
+            server_name, score = user_row
+            message += f"{rank}. {server_name} {score}\n"
+            rank += 1
+        message += "```"
+        await ctx.response.send_message(message)
+
+    @client.tree.command(name="myscore")
+    async def myscore(ctx: discord.Interaction):
+        user = ctx.user
+        QUERY = f"SELECT servername, score, (SELECT COUNT(DISTINCT score) + 1 FROM users WHERE score > u.score) AS rank FROM users u WHERE id = {user.id}"
+        user_rows = db.curr.execute(QUERY).fetchall()
+        message = "```\n"
+        for user_row in user_rows:
+            server_name, score, rank = user_row
+            message += f"{rank}. {server_name} {score}\n"
+            rank += 1
+        message += "```"
+        await ctx.response.send_message(message)
+
+    @client.tree.command(name="meaning")
+    @app_commands.describe(word="Find the meaning of this word")
+    async def meaning(ctx: discord.Interaction, word: str):
+        if word.isalpha():
+            response = requests.get(
+                f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+            )
+            if response.status_code == 400:
+                await ctx.response.send_message("Meaning of this word not found")
+            meaning = response.json()[0]["meanings"][0]["definitions"][0]["definition"]
+            await ctx.response.send_message(f"`{word}: {meaning}`")
+            return
+        await ctx.response.send_message("Invalid word")
+
     client.run(token)
