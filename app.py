72b6d1e4f7b9bbf2ffcfe329a850ca6f4dec48a0
Tejas Mandre <tmandre@vmware.com>
2024-05-03 08:32:08 +0530

    token reinput

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ class App:
 
 
 if __name__ == "__main__":
-    app = App(os.getenv("BOT_TOKEN"))
+    app = App(
+        "MTIyNTQ5MDc1OTQzMjc5ODMyMA.GBK0hM.7cIV6gluTopYAOsmtbYD0hkvmgChVAeYTWJDdg"
+    )
     logger.info("Started Wordchain instance")
     app.run()
