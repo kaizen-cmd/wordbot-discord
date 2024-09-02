60da72695bab9173f7b0ba1c19135921dff5a04f
Tejas Mandre <tmandre3@gmail.com>
2024-09-02 07:40:53 +0000

    get_bot_guilds returns all servers with 5 fails allowed

--- scripts/get_bot_guilds.py
+++ scripts/get_bot_guilds.py
@@ -4,14 +4,30 @@ import requests
 
 
 def get_bot_guilds():
-    url = "https://discord.com/api/v10/users/@me/guilds"
     headers = {"Authorization": f"Bot {os.getenv('BOT_TOKEN')}"}
+    url = "https://discord.com/api/v10/users/@me/guilds"
 
-    response = requests.get(url, headers=headers)
+    server_ids_set = set()
+    last_id = -1
+    result = list()
+    api_call_fails = 0
 
-    if response.status_code == 200:
-        guilds = response.json()
-        return [{"name": guild["name"], "id": guild["id"]} for guild in guilds]
-    else:
-        print(f"Failed to fetch guilds: {response.status_code}, {response.text}")
-        return []
+    while api_call_fails < 5:
+        if last_id == -1:
+            response = requests.get(url, headers=headers)
+        else:
+            response = requests.get(f"{url}?after={last_id}", headers=headers)
+
+        if response.status_code == 200:
+            guilds = response.json()
+            if guilds:
+                last_id = guilds[-1]["id"]
+            if last_id in server_ids_set:
+                break
+            for guild in guilds:
+                server_ids_set.add(guild["id"])
+                result.append({"name": guild["name"], "id": guild["id"]})
+        else:
+            break
+
+    return result
